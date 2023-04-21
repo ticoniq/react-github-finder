b2b674e66de57229d7bb29d977202e14962d9aca
ticoniq_ <okolietochi@gmail>
2023-04-21 21:36:39 +0200

    add loading spinner

--- src/components/layouts/UserResults.jsx
+++ src/components/layouts/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -11,7 +12,7 @@ function UserResults() {
   const fetchUsers = async () => {
     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
       headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        Authorization: `${process.env.REACT_APP_GITHUB_TOKEN}`,
       },
     });
     const data = await response.json();
@@ -23,12 +24,12 @@ function UserResults() {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <h3 key={user.login}>{user.login}</h3>
         ))}
       </div>
     );
   } else {
-    return <h3>Loading...</h3>
+    return <Spinner />;
   }
 }
 
