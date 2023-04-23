8f8ae18ff6ec164bb59bb3775aec68857b189ec5
ticoniq_ <okolietochi@gmail>
2023-04-24 00:58:51 +0200

    cleaning up our repo and axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import Spinner from "../components/layouts/Spinner";
 import RepoList from "../components/repos/RepoList";
 import GithubContext from "../context/github/GithubContext";
-import { getUser, getUserRepos } from "../context/github/GithubAction";
+import { getUserAndRepos } from "../context/github/GithubAction";
 
 function User() {
   const { user, loading, repos, dispatch } = useContext(GithubContext);
@@ -15,11 +15,8 @@ function User() {
   useEffect(() => {
     dispatch({ type: "SET_LOADING" });
     const getUserData = async () => {
-      const userData = await getUser(params.login);
-      dispatch({ type: "GET_USER", payload: userData });
-
-      const userRepoData = await getUserRepos(params.login);
-      dispatch({ type: "GET_REPOS", payload: userRepoData });
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
     };
 
     getUserData();
