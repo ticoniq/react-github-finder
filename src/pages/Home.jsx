ac9e278b4ffa96effdad725b55e49677ff27b4b1
ticoniq_ <okolietochi@gmail>
2023-04-23 07:18:46 +0200

    Create user search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
 
 function Home() {
   return (
     <>
-      {/* Search component  */}
+      <UserSearch />
       <UserResults />
     </>
   );
