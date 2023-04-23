e0c3baa6a849791a01731022c371371c1343d1e9
ticoniq_ <okolietochi@gmail>
2023-04-24 00:16:58 +0200

    repo

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,12 +10,13 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get users search result
+  // Get search results
   const searchUsers = async (text) => {
     setLoading();
 
@@ -59,7 +60,33 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
-  // Clear user from state
+  // Get user repos
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+
+  // Clear users from state
   const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
 
   // Set loading
@@ -71,9 +98,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {children}
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 import { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import Spinner from "../components/layouts/Spinner";
+import RepoList from "../components/repos/RepoList";
 import GithubContext from "../context/github/GithubContext";
 
 function User() {
-  const { getUser, user, loading } = useContext(GithubContext);
+  const { getUser, user, loading, getUserRepos, repos } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
+    getUserRepos(params.login);
     getUser(params.login);
   }, []);
 
@@ -156,6 +158,7 @@ function User() {
             </div>
           </div>
         </div>
+        <RepoList repos={repos} />
       </div>
     </>
   );
