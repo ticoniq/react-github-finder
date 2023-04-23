430af13a9790c4750848b60d61ac82bfcbeb48fa
ticoniq_ <okolietochi@gmail>
2023-04-23 07:52:23 +0200

    updated user serch component

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,18 +4,20 @@ import GithubContext from "../../context/github/GithubContext";
 function UserSearch() {
   const [text, setText] = useState("");
 
-  const { users } = useContext(GithubContext);
+  const { users, searchUsers } = useContext(GithubContext);
 
   const handleChange = (e) => setText(e.target.value);
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+
     if (text === "") {
       alert("Please enter something");
     } else {
-      // Todo search users
+      searchUsers(text);
+
       setText("");
     }
-    e.preventDefault();
   };
 
   return (
